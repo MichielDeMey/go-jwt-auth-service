5774f711c2bfbbffb73ef41f1e4a75ea88def644
Michiel De Mey <de.mey.michiel@gmail.com>
2017-06-27 21:06:19 +0200

    email address updates

--- etc/sql/003_create_user.sql
+++ etc/sql/003_create_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO "public"."users"("email", "password") VALUES('de.mey.michiel@gmail.com', crypt('test123', gen_salt('bf', 8))) RETURNING "id", "email", "password";
-INSERT INTO "public"."users"("email", "password") VALUES('bino@vito.be', crypt('test456', gen_salt('bf', 8))) RETURNING "id", "email", "password";
+INSERT INTO "public"."users"("email", "password") VALUES('michiel@demey.io', crypt('test123', gen_salt('bf', 8))) RETURNING "id", "email", "password";
+INSERT INTO "public"."users"("email", "password") VALUES('gilles@demey.io', crypt('test456', gen_salt('bf', 8))) RETURNING "id", "email", "password";
